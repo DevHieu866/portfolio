9a6e9af31a65cd7341aee532f6ffc9459e3d88ad
DevHieu866 <hieu.t.tran775@gmail.com>
2025-01-06 16:26:04 -0800

    Changed env

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -9,7 +9,8 @@ export default function ContactForm(){
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    formData.append("access_key","3b483299-694e-4df1-ab4c-6fccbb8db389")
+    const emailApiKey = process.env.EMAIL_API_KEY;
+    formData.append("access_key","emailApiKey")
     const object = Object.fromEntries(formData)
     const json = JSON.stringify(object)
     const data = Object.fromEntries(formData.entries());
